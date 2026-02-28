6fb9a75b9b61073e2e143c5d8bfc47335b362d3f
dushyant2006 <dushyant50003@gmail.com>
2026-02-28 15:46:18 +0530

    feat: initialized serial communication and RGB pin configuration

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,22 @@
  * @author Dushyant
  * @date 2026-02-28
  */
+#define RED_PIN 9
+#define GREEN_PIN 10
+#define BLUE_PIN 11
 
-void setup() {
-}
+
+    void setup() {
+
+        Serial.begin(9600);
+
+        pinMode(RED_PIN, OUTPUT);
+        pinMode(GREEN_PIN, OUTPUT);
+        pinMode(BLUE_PIN, OUTPUT);
+
+        Serial.println("RGB LED Control Initialized");
+    }
+
 
 void loop() {
 }
